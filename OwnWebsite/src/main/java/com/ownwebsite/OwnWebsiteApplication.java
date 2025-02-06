2f7b044aabbaee4b007dafe0c57142faa1d40ab8
DaanWeer <daan.weerheim@odido.nl>
2025-02-06 11:31:40 +0100

    first commit

---
+++ OwnWebsite/src/main/java/com/ownwebsite/OwnWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.ownwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OwnWebsiteApplication.class, args);
+    }
+
+}
---
+++ OwnWebsite/src/test/java/com/ownwebsite/OwnWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ownwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
