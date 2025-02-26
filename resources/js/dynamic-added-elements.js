bf119d8af70399250289b7aa90d8f74d777b211b
DaanWeer <daan.weerheim@odido.nl>
2025-02-26 17:37:08 +0100

    implements dynamic element adding

---
+++ resources/js/dynamic-added-elements.js
@@ -0,0 +1,19 @@
+// This script has helper functions that allow dyncamic adding of elements and content on multiple pages
+
+function loadComponent(id, file) {
+    fetch(file)
+        .then(response => response.text())
+        .then(data => document.getElementById(id).innerHTML = data);
+}
+
+async function loadHeaderAndFooter() {
+    await Promise.all([
+        loadComponent("header", "header.html"),
+        loadComponent("footer", "footer.html")
+    ]);
+}
+
+// DOMContentLoaded event waits for page to be parsed and the fires below loadComponent function
+// async function tells javascript there are asynchronous operations allows to use await
+
+document.addEventListener("DOMContentLoaded", loadHeaderAndFooter);
